d4eb5c9df135fa4f53c94fe06476bd7a75e63755
Matthew Chick <chikmatthew@gmail.com>
2017-12-14 20:11:17 +1100

    how to use var to declare the variable and := for initialization

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var card string = "Ace of Spades" // static type
+	card := "Ace of Spades" //initialize
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
