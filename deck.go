6bd4851fd6e9bee536be9e310081584205a9a273
Matthew Chick <chikmatthew@gmail.com>
2017-12-15 16:06:31 +1100

    Create newDeck() and how to call it

--- deck.go
+++ deck.go
@@ -7,6 +7,23 @@ import "fmt"
 
 type deck []string
 
+// return deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+// receiver
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- main.go
+++ main.go
@@ -2,25 +2,22 @@
 
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// var card string = "Ace of Spades" // static type
-	cards := deck{"Ace of Diamonds", newCard()} //initializeby by a slice of type string
-
+	// cards := deck{"Ace of Diamonds", newCard()} //initializeby by a slice of type string
 	// card = "Five of Diamonds"   //reassignment
-	cards = append(cards, "Six of Spades") //not modify the slice
+	// cards = append(cards, "Six of Spades") //not modify the slice
+
+	cards := newDeck()
 
 	cards.print() //call print function from deck.go
 
-	for _, card := range cards {
-		fmt.Println(card)
-	}
+	// for _, card := range cards {
+	// 	fmt.Println(card)
+	// }
 
 }
 
-func newCard() string { //return a string
-	return "Five of Diamonds"
-}
+// func newCard() string { //return a string
+// 	return "Five of Diamonds"
+// }
