b83583d904984c46a679be2b8b12260060f71fe7
Matthew Chick <chikmatthew@gmail.com>
2017-12-15 13:48:45 +1100

    how to use function

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 
 func main() {
 	// var card string = "Ace of Spades" // static type
-	card := "Ace of Spades" //initialize
-	card = "Five of Diamonds"
+	card := newCard() //initialize
+	// card = "Five of Diamonds"   //reassignment
 
 	fmt.Println(card)
+}
+
+func newCard() string { //return a string
+	return "Five of Diamonds"
 }
